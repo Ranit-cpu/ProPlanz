90693985250ef9df4965d0de6dddb10fb32fc042
Ranit <ranits003@gmail.com>
2025-02-26 03:28:05 +0530

    initial commit

---
+++ src/main/java/com/Spring/ProPlanz.java
@@ -0,0 +1,12 @@
+package com.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPlanz {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPlanz.class, args);
+	}
+}
