2bc19652bb31590bbae0995e40f47f3fa897e64d
Ranit Saha <ranits003@gmail.com>
2025-02-26 14:22:18 +0530

    Update MainController.java

--- src/main/java/com/Spring/controller/MainController.java
+++ src/main/java/com/Spring/controller/MainController.java
@@ -1,1 +1,39 @@
+package com.Spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Controller
+public class MainController {
+      
+      @GetMapping("/")
+      public String home(){
+            return"index";
+      }
+      
+      @PostMapping("/calculate")
+      public String Calculate(@RequestParam int num1, @RequestParam int num2, Model model) {
+            
+            try{
+                  ProcessBuilder process = new ProcessBuilder("python3","/Users/ranitsaha/Developer/java/ProPlanz/script.py",String.valueOf(num1),
+                          String.valueOf(num2));
+                  process.redirectErrorStream(true);
+                  
+                  Process pros = process.start();
+                  
+                  BufferedReader read = new BufferedReader(new InputStreamReader(pros.getInputStream()));
+                  String result = read.readLine();
+                  pros.waitFor();
+
+                  model.addAttribute("result","Result from python : "+result);
+            } catch (Exception e) {
+                  model.addAttribute("result","Error" + e.getMessage());
+            }
+            return "index";
+      }
+}
